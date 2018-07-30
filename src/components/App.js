3d1a983749b13f09694d1e7f2fdaa26f8a865bb9
Victor Velasquez <victorv24@gmail.com>
2018-07-30 16:42:52 -0700

    main content added

--- src/components/App.js
+++ src/components/App.js
@@ -6,33 +6,41 @@ class App extends Component {
   render() {
     return (
       <div>
-      	
+      	{/* Header */}
 				<div className="header">
 					<div className="header__content">
-						
-						<div className="logo"></div>
-						
+						{/* Logo */}
+						<div className="logo">
+							<i class="fa fa-github">Logo</i>
+						</div>
+						{/* Search bar */}
 						<div className="search-bar">
 							<input type="text"/>Search or jump to...
 						</div>
-						
-						<ul className="menu">
+						{/* Middle of nav */}
+						<ul className="menu-left">
 							<li className="">Pull requests</li>
 							<li className="">Issues</li>
 							<li className="">Marketplace</li>
 							<li className="">Explore</li>
 						</ul>
-
-						<ul>
+						{/* Right side of nav */}
+						<ul className="menu-right">
 							<li>Bell</li>
 							<li>Add Dropdown</li>
 							<li>Profile Dropdown</li>
 						</ul>
-
 					</div>
 				</div>
-				
-				<div></div>
+				{/* Profile section */}
+				<div className="profile-section">
+					<div className="profile-photo">
+						<img src="" alt=""/>My Photo
+					</div>
+					<div className="name">My Name</div>
+					<div className="Bio">This is my convoluted bio.</div>
+					<button>Follow</button>
+				</div>
 
 				<RepoCo />
       </div>
--- src/components/RepoCo.js
+++ src/components/RepoCo.js
@@ -3,7 +3,31 @@ import React, { Component } from 'react'
 class RepoCo extends Component {
  render() {
    return (
-		<div></div>
+		<div>
+			{/* Tabs menu */}
+			<div>
+				<ul className="tabs">
+					<li className="tab-links">Overview</li>
+					<li className="tab-links">Repositories</li>
+					<li className="tab-links">Stars</li>
+					<li className="tab-links">Followers</li>
+					<li className="tab-links">Following</li>
+				</ul>
+			</div>
+			{/* Search and Repo list */}
+			<div>
+				{/* Search and filter */}
+				<div>
+					<input type="text"/>
+				</div>
+				<button>Type: All</button>
+				<button>Language: All</button>
+				<button>New</button>
+				{/* Repo List */}
+				<div>These are my repos</div>
+			</div>
+
+		</div>
    )
  }
 }
