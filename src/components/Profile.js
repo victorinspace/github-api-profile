1078ccc22b0ea1e5399e3a88ed3c1cbdf3308084
Victor Velasquez <victorv24@gmail.com>
2018-08-19 14:03:15 -0700

    refactoring code

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { getInfo } from '../actions/pageFunctions'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { Provider } from 'react-redux'
+import store from '../store'
 
 import Repo from './Repo'
 
@@ -12,59 +14,62 @@ class Profile extends Component {
 
 	render() {
 		return (
-			<Router>
-				<div>
-					<div className="top-nav">
-						<div className="top-nav--left">
-							<i id="github-icon" className="fa fa-github"></i>
-							<input className="profile-search" type="text" 
-											placeholder="Search or jump to..." />
-							<div className="top-nav__text">Pull requests</div>
-							<div className="top-nav__text">Issues</div>
-							<div className="top-nav__text">Marketplace</div>
-							<div className="top-nav__text">Example</div>
-						</div>
+			<Provider store={store}>
+				<Router>
+					<div>
+						<div className="main-container">
+							<div id="top-nav" className="top-nav">
+								<div className="top-nav--left">
+									<i id="github-icon" className="fa fa-github"></i>
+									<input className="profile-search" type="text" 
+													placeholder="Search or jump to..." />
+									<div className="top-nav__text">Pull requests</div>
+									<div className="top-nav__text">Issues</div>
+									<div className="top-nav__text">Marketplace</div>
+									<div className="top-nav__text">Example</div>
+								</div>
 
-						<div className="top-nav--right">
-							<i id="bell-icon" className="fa fa-bell"></i>
-							<i id="plus-icon" className="fa fa-plus"></i>
-							<img className="top-avatar" src={this.props.avatar} alt="" />
-						</div>
-					</div>
+								<div className="top-nav--right">
+									<i id="bell-icon" className="fa fa-bell"></i>
+									<i id="plus-icon" className="fa fa-plus"></i>
+									<img className="top-avatar" src={this.props.avatar} alt="" />
+								</div>
+							</div>
 
-					<div className="container">
-						<div className="profile">
-							<img className="main-avatar" src={this.props.avatar} alt="" />
-							<div className="name">{this.props.name}</div>
-							<div className="profile-name">{this.props.login}</div>
-							<button className="bio">Add a bio</button>
-						</div>
+							<div className="profile-container">
+								<div className="profile">
+									<img className="main-avatar" src={this.props.avatar} alt="" />
+									<div className="name">{this.props.name}</div>
+									<div className="profile-name">{this.props.login}</div>
+									<button className="bio">Add a bio</button>
+								</div>
 
-						<div className="data">
-							<div className="tab-container">
-								<div className="tab-container-space">
-									<Link className="tabs" to="">
-										<div>Overview</div>
-									</Link>
-									<Link className="tabs" to="">
-										<div>Repositories {this.props.publicrepo}</div>
-									</Link>
-									<Link className="tabs" to="">
-										<div>Stars</div>
-									</Link>
-									<Link className="tabs" to="">
-										<div>Followers {this.props.followers}</div>
-									</Link>
-									<Link className="tabs" to="">
-										<div>Following {this.props.following}</div>
-									</Link>
+								<div className="data">
+									<div className="tab-container">
+										<div className="tab-container-space">
+											<Link className="tabs" to="">
+												<div>Overview</div>
+											</Link>
+											<Link className="tabs" to="">
+												<div>Repositories {this.props.publicrepo}</div>
+											</Link>
+											<Link className="tabs" to="">
+												<div>Stars</div>
+											</Link>
+											<Link className="tabs" to="">
+												<div>Followers {this.props.followers}</div>
+											</Link>
+											<Link className="tabs" to="">
+												<div>Following {this.props.following}</div>
+											</Link>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
-				</div>
-			</Router>
+				</Router>
+			</Provider>
 		)
 	}
 }
